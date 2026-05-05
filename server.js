0f0c09d970bf7170811a91006d5680cbf3c6dc17
ramyabrato <debramyabrato@gmail.comm>
2026-05-05 12:29:06 +0530

    feat: add request logging to server and improve error/session handling in admin UI

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname)));
 
+// Simple Request Logger
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    next();
+});
+
 // Middleware for Auth
 const authenticateToken = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
